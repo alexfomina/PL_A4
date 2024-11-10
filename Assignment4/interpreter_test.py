ed48fe2482cf04fa705fedd0fffa6892a4e90d9f
Alexandra Fomina <fomina@chapman.edu>
2024-11-10 15:31:32 -0800

    Updating interpreter_test.py

--- Assignment4/interpreter_test.py
+++ Assignment4/interpreter_test.py
@@ -6,7 +6,6 @@ from colorama import Fore, Style
 def print_trees(source_code):
     print("Source code:", source_code); print()
     cst = parser.parse(source_code)
-    #print("CST:", cst); print()
     ast = LambdaCalculusTransformer().transform(cst)
     print("AST:", ast); print()
     print("===\n")
@@ -24,107 +23,190 @@ def test_parse():
     MAGENTA = '\033[95m'
     RESET = '\033[0m'
     
-    assert ast(r"x") == ('var', 'x')
-    print(f"AST {MAGENTA}x{RESET} == ('var', 'x')")
+    # Existing tests...
     
-    assert ast(r"(((x)) ((y)))") == ('app', ('var', 'x'), ('var', 'y'))
-    print(f"AST {MAGENTA}(((x)) ((y))){RESET} == ('app', ('var', 'x'), ('var', 'y'))")
+    # New tests (highlighted in blue)
+    BLUE = Fore.BLUE + Style.BRIGHT
+    print(f"{BLUE}AST for addition: {MAGENTA}x + y{RESET} == ('add', ('var', 'x'), ('var', 'y'))")
+    assert ast(r"x + y") == ('add', ('var', 'x'), ('var', 'y'))
     
-    assert ast(r"x y") == ('app', ('var', 'x'), ('var', 'y'))
-    print(f"AST {MAGENTA}x y{RESET} == ('app', ('var', 'x'), ('var', 'y'))")
+    print(f"{BLUE}AST for subtraction: {MAGENTA}x - y{RESET} == ('sub', ('var', 'x'), ('var', 'y'))")
+    assert ast(r"x - y") == ('sub', ('var', 'x'), ('var', 'y'))
     
-    assert ast(r"x y z") == ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z'))
-    print(f"AST {MAGENTA}x y z{RESET} == ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z'))")
+    print(f"{BLUE}AST for multiplication: {MAGENTA}x * y{RESET} == ('mul', ('var', 'x'), ('var', 'y'))")
+    assert ast(r"x * y") == ('mul', ('var', 'x'), ('var', 'y'))
     
-    assert ast(r"\x.y") == ('lam', 'x', ('var', 'y'))
-    print(f"AST {MAGENTA}\\x.y{RESET} == ('lam', 'x', ('var', 'y'))")
+    print(f"{BLUE}AST for negation: {MAGENTA}-x{RESET} == ('neg', ('var', 'x'))")
+    assert ast(r"-x") == ('neg', ('var', 'x'))
     
-    assert ast(r"\x.x y") == ('lam', 'x', ('app', ('var', 'x'), ('var', 'y')))
-    print(f"AST {MAGENTA}\\x.x y{RESET} == ('lam', 'x', ('app', ('var', 'x'), ('var', 'y')))")
+    print(f"{BLUE}AST for exponentiation: {MAGENTA}x ^ y{RESET} == ('power', ('var', 'x'), ('var', 'y'))")
+    assert ast(r"x ^ y") == ('power', ('var', 'x'), ('var', 'y'))
     
-    assert ast(r"\x.x y z") == ('lam', 'x', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))
-    print(f"AST {MAGENTA}\\x.x y z{RESET} == ('lam', 'x', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))")
-    
-    assert ast(r"\x. \y. \z. x y z") == ('lam', 'x', ('lam', 'y', ('lam', 'z', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))))
-    print(f"AST {MAGENTA}\\x. \\y. \\z. x y z{RESET} == ('lam', 'x', ('lam', 'y', ('lam', 'z', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))))")
-    
-    assert ast(r"\x. x a") == ('lam', 'x', ('app', ('var', 'x'), ('var', 'a')))
-    print(f"AST {MAGENTA}\\x. x a{RESET} == ('lam', 'x', ('app', ('var', 'x'), ('var', 'a')))")
-    
-    assert ast(r"\x. x (\y. y)") == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('var', 'y'))))
-    print(f"AST {MAGENTA}\\x. x (\\y. y){RESET} == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('var', 'y'))))")
-    
-    assert ast(r"\x. x (\y. y (\z. z z2))") == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('app', ('var', 'y'), ('lam', 'z', ('app', ('var', 'z'), ('var', 'z2')))))))
-    print(f"AST {MAGENTA}\\x. x (\\y. y (\\z. z z2)){RESET} == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('app', ('var', 'y'), ('lam', 'z', ('app', ('var', 'z'), ('var', 'z2')))))))")
-    
-    assert ast(r"\x. y z (\a. b (\c. d e f))") == ('lam', 'x', ('app', ('app', ('var', 'y'), ('var', 'z')), ('lam', 'a', ('app', ('var', 'b'), ('lam', 'c', ('app', ('app', ('var', 'd'), ('var', 'e')), ('var', 'f')))))))
-    print(f"AST {MAGENTA}\\x. y z (\\a. b (\\c. d e f)){RESET} == ('lam', 'x', ('app', ('app', ('var', 'y'), ('var', 'z')), ('lam', 'a', ('app', ('var', 'b'), ('lam', 'c', ('app', ('app', ('var', 'd'), ('var', 'e')), ('var', 'f')))))))")
+    print(f"{BLUE}AST for logarithm: {MAGENTA}log(x){RESET} == ('log', ('var', 'x'))")
+    assert ast(r"log(x)") == ('log', ('var', 'x'))
     
     print("\nParser: All tests passed!\n")
-
-def test_substitute():
-    MAGENTA = '\033[95m'
-    RESET = '\033[0m'
-    
-    # x [y/x] = y
-    assert substitute(('var', 'x'), 'x', ('var', 'y')) == ('var', 'y')
-    print(f"SUBST {MAGENTA}x [y/x]{RESET} == ('var', 'y')")
-    
-    # \x.x [y/x] = (\x.x)
-    assert substitute(('lam', 'x', ('var', 'x')), 'x', ('var', 'y')) == ('lam', 'x', ('var', 'x'))
-    print(f"SUBST {MAGENTA}\\x.x [y/x]{RESET} == ('lam', 'x', ('var', 'x'))")
-    
-    # (x x) [y/x] = y y
-    assert substitute(('app', ('var', 'x'), ('var', 'x')), 'x', ('var', 'y')) == ('app', ('var', 'y'), ('var', 'y'))
-    print(f"SUBST {MAGENTA}(x x) [y/x]{RESET} == ('app', ('var', 'y'), ('var', 'y'))")
-    
-    # (\y. x ) [y/x] = (\Var1. y)
-    assert substitute(('lam', 'y', ('var', 'x')), 'x', ('var', 'y')) == ('lam', 'Var1', ('var', 'y'))
-    print(f"SUBST {MAGENTA}\\y. x [y/x]{RESET} == ('lam', 'Var1', ('var', 'y'))")
-
-    print("\nsubstitute(): All tests passed!\n")
 
 def test_evaluate():
     MAGENTA = '\033[95m'
     RESET = '\033[0m'
     
-    # EVAL x == x
-    assert linearize(evaluate(ast(r"x"))) == "x"
-    print(f"EVAL {MAGENTA}x{RESET} == x")
+    # Existing evaluation tests...
     
-    # EVAL x y == (x y)
-    assert linearize(evaluate(ast(r"x y"))) == "(x y)"
-    print(f"EVAL {MAGENTA}x y{RESET} == (x y)")
-    
-    # EVAL x y z == ((x y) z)
-    assert linearize(evaluate(ast(r"x y z"))) == "((x y) z)"
-    print(f"EVAL {MAGENTA}x y z{RESET} == ((x y) z)")
-    
-    # EVAL x (y z) == (x (y z))
-    assert linearize(evaluate(ast(r"x (y z)"))) == "(x (y z))"
-    print(f"EVAL {MAGENTA}x (y z){RESET} == (x (y z))")
-    
-    # EVAL \x.y == \x.y
-    assert linearize(evaluate(ast(r"\x.y"))) == r"(\x.y)"
-    print(f"EVAL {MAGENTA}\\x.y{RESET} == \\x.y")
-    
-    # EVAL (\x.x) y == y
-    assert linearize(evaluate(ast(r"(\x.x) y"))) == "y"
-    print(f"EVAL {MAGENTA}(\\x.x) y{RESET} == y")
+    # New evaluation tests (highlighted in blue)
+    BLUE = Fore.BLUE + Style.BRIGHT
+    assert linearize(evaluate(ast(r"3 + 4"))) == "7"
+    print(f"{BLUE}EVAL {MAGENTA}3 + 4{RESET} == 7")
+
+    assert linearize(evaluate(ast(r"5 - 2"))) == "3"
+    print(f"{BLUE}EVAL {MAGENTA}5 - 2{RESET} == 3")
+
+    assert linearize(evaluate(ast(r"6 * 7"))) == "42"
+    print(f"{BLUE}EVAL {MAGENTA}6 * 7{RESET} == 42")
+
+    assert linearize(evaluate(ast(r"-5"))) == "-5"
+    print(f"{BLUE}EVAL {MAGENTA}-5{RESET} == -5")
+
+    assert linearize(evaluate(ast(r"2 ^ 3"))) == "8"
+    print(f"{BLUE}EVAL {MAGENTA}2 ^ 3{RESET} == 8")
+
+    assert linearize(evaluate(ast(r"log(1)"))) == "0"
+    print(f"{BLUE}EVAL {MAGENTA}log(1){RESET} == 0")
 
     print("\nevaluate(): All tests passed!\n")
 
-def test_interpret():
-    print(f"Testing x --> {interpret('x')}")
-    print(f"Testing x y --> {interpret('x y')}")
-    input=r"\x.x"; output = interpret(input); print(f"Testing {input} --> {output}")
-    input=r"(\x.x) y"; output = interpret(input); print(f"Testing {input} --> {output}")
-    input=r"(\x.\y.x y) y"; output = interpret(input); print(f"Testing {input} --> {output}")
-
-    print("\ninterpret(): All tests passed!\n")
-
 if __name__ == "__main__":
     print(Fore.GREEN + "\nTEST PARSING\n" + Style.RESET_ALL); test_parse()
-    print(Fore.GREEN + "\nTEST SUBSTITUTION\n" + Style.RESET_ALL); test_substitute()
     print(Fore.GREEN + "\nTEST EVALUATION\n" + Style.RESET_ALL); test_evaluate()
-    print(Fore.GREEN + "\nTEST INTERPRETATION\n" + Style.RESET_ALL); test_interpret()
+
+
+# from interpreter import interpret, substitute, evaluate, LambdaCalculusTransformer, parser, linearize
+# from lark import Lark, Transformer
+# from colorama import Fore, Style
+
+# # for testing the grammar, the parser and the conversion to ASTs
+# def print_trees(source_code):
+#     print("Source code:", source_code); print()
+#     cst = parser.parse(source_code)
+#     #print("CST:", cst); print()
+#     ast = LambdaCalculusTransformer().transform(cst)
+#     print("AST:", ast); print()
+#     print("===\n")
+
+# # convert concrete syntax to AST
+# def ast(source_code):
+#     return LambdaCalculusTransformer().transform(parser.parse(source_code))
+
+# def print_ast(source_code):
+#     print()
+#     print("AST:", ast(source_code))
+#     print()
+
+# def test_parse():
+#     MAGENTA = '\033[95m'
+#     RESET = '\033[0m'
+    
+#     assert ast(r"x") == ('var', 'x')
+#     print(f"AST {MAGENTA}x{RESET} == ('var', 'x')")
+    
+#     assert ast(r"(((x)) ((y)))") == ('app', ('var', 'x'), ('var', 'y'))
+#     print(f"AST {MAGENTA}(((x)) ((y))){RESET} == ('app', ('var', 'x'), ('var', 'y'))")
+    
+#     assert ast(r"x y") == ('app', ('var', 'x'), ('var', 'y'))
+#     print(f"AST {MAGENTA}x y{RESET} == ('app', ('var', 'x'), ('var', 'y'))")
+    
+#     assert ast(r"x y z") == ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z'))
+#     print(f"AST {MAGENTA}x y z{RESET} == ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z'))")
+    
+#     assert ast(r"\x.y") == ('lam', 'x', ('var', 'y'))
+#     print(f"AST {MAGENTA}\\x.y{RESET} == ('lam', 'x', ('var', 'y'))")
+    
+#     assert ast(r"\x.x y") == ('lam', 'x', ('app', ('var', 'x'), ('var', 'y')))
+#     print(f"AST {MAGENTA}\\x.x y{RESET} == ('lam', 'x', ('app', ('var', 'x'), ('var', 'y')))")
+    
+#     assert ast(r"\x.x y z") == ('lam', 'x', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))
+#     print(f"AST {MAGENTA}\\x.x y z{RESET} == ('lam', 'x', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))")
+    
+#     assert ast(r"\x. \y. \z. x y z") == ('lam', 'x', ('lam', 'y', ('lam', 'z', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))))
+#     print(f"AST {MAGENTA}\\x. \\y. \\z. x y z{RESET} == ('lam', 'x', ('lam', 'y', ('lam', 'z', ('app', ('app', ('var', 'x'), ('var', 'y')), ('var', 'z')))))")
+    
+#     assert ast(r"\x. x a") == ('lam', 'x', ('app', ('var', 'x'), ('var', 'a')))
+#     print(f"AST {MAGENTA}\\x. x a{RESET} == ('lam', 'x', ('app', ('var', 'x'), ('var', 'a')))")
+    
+#     assert ast(r"\x. x (\y. y)") == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('var', 'y'))))
+#     print(f"AST {MAGENTA}\\x. x (\\y. y){RESET} == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('var', 'y'))))")
+    
+#     assert ast(r"\x. x (\y. y (\z. z z2))") == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('app', ('var', 'y'), ('lam', 'z', ('app', ('var', 'z'), ('var', 'z2')))))))
+#     print(f"AST {MAGENTA}\\x. x (\\y. y (\\z. z z2)){RESET} == ('lam', 'x', ('app', ('var', 'x'), ('lam', 'y', ('app', ('var', 'y'), ('lam', 'z', ('app', ('var', 'z'), ('var', 'z2')))))))")
+    
+#     assert ast(r"\x. y z (\a. b (\c. d e f))") == ('lam', 'x', ('app', ('app', ('var', 'y'), ('var', 'z')), ('lam', 'a', ('app', ('var', 'b'), ('lam', 'c', ('app', ('app', ('var', 'd'), ('var', 'e')), ('var', 'f')))))))
+#     print(f"AST {MAGENTA}\\x. y z (\\a. b (\\c. d e f)){RESET} == ('lam', 'x', ('app', ('app', ('var', 'y'), ('var', 'z')), ('lam', 'a', ('app', ('var', 'b'), ('lam', 'c', ('app', ('app', ('var', 'd'), ('var', 'e')), ('var', 'f')))))))")
+    
+#     print("\nParser: All tests passed!\n")
+
+# def test_substitute():
+#     MAGENTA = '\033[95m'
+#     RESET = '\033[0m'
+    
+#     # x [y/x] = y
+#     assert substitute(('var', 'x'), 'x', ('var', 'y')) == ('var', 'y')
+#     print(f"SUBST {MAGENTA}x [y/x]{RESET} == ('var', 'y')")
+    
+#     # \x.x [y/x] = (\x.x)
+#     assert substitute(('lam', 'x', ('var', 'x')), 'x', ('var', 'y')) == ('lam', 'x', ('var', 'x'))
+#     print(f"SUBST {MAGENTA}\\x.x [y/x]{RESET} == ('lam', 'x', ('var', 'x'))")
+    
+#     # (x x) [y/x] = y y
+#     assert substitute(('app', ('var', 'x'), ('var', 'x')), 'x', ('var', 'y')) == ('app', ('var', 'y'), ('var', 'y'))
+#     print(f"SUBST {MAGENTA}(x x) [y/x]{RESET} == ('app', ('var', 'y'), ('var', 'y'))")
+    
+#     # (\y. x ) [y/x] = (\Var1. y)
+#     assert substitute(('lam', 'y', ('var', 'x')), 'x', ('var', 'y')) == ('lam', 'Var1', ('var', 'y'))
+#     print(f"SUBST {MAGENTA}\\y. x [y/x]{RESET} == ('lam', 'Var1', ('var', 'y'))")
+
+#     print("\nsubstitute(): All tests passed!\n")
+
+# def test_evaluate():
+#     MAGENTA = '\033[95m'
+#     RESET = '\033[0m'
+    
+#     # EVAL x == x
+#     assert linearize(evaluate(ast(r"x"))) == "x"
+#     print(f"EVAL {MAGENTA}x{RESET} == x")
+    
+#     # EVAL x y == (x y)
+#     assert linearize(evaluate(ast(r"x y"))) == "(x y)"
+#     print(f"EVAL {MAGENTA}x y{RESET} == (x y)")
+    
+#     # EVAL x y z == ((x y) z)
+#     assert linearize(evaluate(ast(r"x y z"))) == "((x y) z)"
+#     print(f"EVAL {MAGENTA}x y z{RESET} == ((x y) z)")
+    
+#     # EVAL x (y z) == (x (y z))
+#     assert linearize(evaluate(ast(r"x (y z)"))) == "(x (y z))"
+#     print(f"EVAL {MAGENTA}x (y z){RESET} == (x (y z))")
+    
+#     # EVAL \x.y == \x.y
+#     assert linearize(evaluate(ast(r"\x.y"))) == r"(\x.y)"
+#     print(f"EVAL {MAGENTA}\\x.y{RESET} == \\x.y")
+    
+#     # EVAL (\x.x) y == y
+#     assert linearize(evaluate(ast(r"(\x.x) y"))) == "y"
+#     print(f"EVAL {MAGENTA}(\\x.x) y{RESET} == y")
+
+#     print("\nevaluate(): All tests passed!\n")
+
+# def test_interpret():
+#     print(f"Testing x --> {interpret('x')}")
+#     print(f"Testing x y --> {interpret('x y')}")
+#     input=r"\x.x"; output = interpret(input); print(f"Testing {input} --> {output}")
+#     input=r"(\x.x) y"; output = interpret(input); print(f"Testing {input} --> {output}")
+#     input=r"(\x.\y.x y) y"; output = interpret(input); print(f"Testing {input} --> {output}")
+
+#     print("\ninterpret(): All tests passed!\n")
+
+# if __name__ == "__main__":
+#     print(Fore.GREEN + "\nTEST PARSING\n" + Style.RESET_ALL); test_parse()
+#     print(Fore.GREEN + "\nTEST SUBSTITUTION\n" + Style.RESET_ALL); test_substitute()
+#     print(Fore.GREEN + "\nTEST EVALUATION\n" + Style.RESET_ALL); test_evaluate()
+#     print(Fore.GREEN + "\nTEST INTERPRETATION\n" + Style.RESET_ALL); test_interpret()
